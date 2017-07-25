0a742cc7a014475b16d416a02e54937cdba88a69
Leander Seefeld <leanseefeld@gmail.com>
2017-07-25 15:16:12 +1200

    update gulp to use webpack

--- gulpfile.js
+++ gulpfile.js
@@ -4,70 +4,23 @@ let gulp = require('gulp'),
   pump = require('pump'),
   sass = require('gulp-sass');
 
-const DEST = 'dist',
-  SASS_GLOB = 'src/style/**/*.scss',
-  IMG_GLOB = 'src/img/*.*',
-  HTML_BLOB = 'src/**/*.html';
-
-gulp.task('sass', function sassTask() {
-  return pump([
-    gulp.src(SASS_GLOB),
-    sass().on('error', sass.logError),
-    gulp.dest(DEST + '/style')
-  ]);
-});
-
-gulp.task('sass:watch', function sassWatch() {
-  gulp.watch(SASS_GLOB, ['sass']);
-});
-
-gulp.task('images', function imgTask() {
-  return pump([
-    gulp.src(IMG_GLOB),
-    gulp.dest(DEST + '/img')
-  ]);
-});
-
-gulp.task('images:watch', function imagesWatch() {
-  gulp.watch(IMG_GLOB, ['images']);
-});
-
-gulp.task('html', function htmlTask() {
-  return pump([
-    gulp.src(HTML_BLOB),
-    gulp.dest(DEST)
-  ]);
-});
-
-gulp.task('html:watch', function htmlWatch() {
-  gulp.watch(HTML_BLOB, ['html']);
-});
+const DEST = 'dist';
 
 gulp.task('clean', function cleanTask() {
   let del = require('del');
-  return del([DEST], {
+  return del([
+    DEST + '/**',
+    '!' + DEST
+  ], {
     force: true
   });
 });
 
 gulp.task('dist', ['clean'], function distTask(cb) {
-  let runSequence = require('run-sequence');
-  runSequence([
-      'images',
-      'sass',
-      'html'
-    ], //
-    function(result) {
-      if (result) {
-        console.error(result);
-      } else {
-        console.log('Dist finished');
-      }
-      cb(result);
-    });
+  let webpack = require('webpack'),
+    config = require('./webpack.config');
+  return webpack(config, cb);
 });
-
-gulp.task('watch', ['images:watch', 'sass:watch', 'html:watch']);
 
 const deploy = function deploy(force) {
   let ftp = require('vinyl-ftp'),
