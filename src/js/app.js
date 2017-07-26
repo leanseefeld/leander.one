897420725801747b0a64651046f0b219da88dd17
Leander Seefeld <leanseefeld@gmail.com>
2017-07-26 16:50:13 +1200

    menu toggle behaviour and animation

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,16 @@
 import '../style/master.scss';
+
+const openMenuClass = 'open';
+
+let sideMenuEl = document.querySelector('.side-menu'),
+  menuToggleEl = document.querySelector('.menu-button');
+
+menuToggleEl.onclick = function toggleMenu() {
+  let classes = sideMenuEl.className;
+  let toggleIndex = classes.indexOf(openMenuClass);
+  if (toggleIndex >= 0) {
+    sideMenuEl.className = classes.replace(openMenuClass, '');
+  } else {
+    sideMenuEl.className = classes + ' ' + openMenuClass;
+  }
+};
